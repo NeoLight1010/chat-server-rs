3d5ecf78af9022f20a5f149ec3d3b0f8d9facfdd
Anthony Suárez <neolight1010@gmail.com>
2022-11-12 18:20:38 -0500

    Make echo server multi-client

--- src/main.rs
+++ src/main.rs
@@ -13,38 +13,42 @@ async fn main() {
 
     println!("Listening on {addr}.");
 
-    let (mut socket, addr) = listener
-        .accept()
-        .await
-        .expect("Error accepting connection.");
-
-    let (read_socket_half, mut write_socket_half) = socket.split();
-
-    println!(
-        "Opened connection with {} on port {}.",
-        addr.ip(),
-        addr.port()
-    );
-
-    let mut reader = BufReader::new(read_socket_half);
-    let mut line = String::new();
-
     loop {
-        let bytes_read = reader
-            .read_line(&mut line)
+        let (mut socket, addr) = listener
+            .accept()
             .await
-            .expect("Error reading data.");
+            .expect("Error accepting connection.");
 
-        if bytes_read == 0 {
-            println!("Closed connection.");
-            break;
-        }
+        tokio::spawn(async move {
+            let (read_socket_half, mut write_socket_half) = socket.split();
 
-        write_socket_half
-            .write_all(line.as_bytes())
-            .await
-            .expect("Error echoing data.");
+            println!(
+                "Opened connection with {} on port {}.",
+                addr.ip(),
+                addr.port()
+            );
 
-        line.clear();
+            let mut reader = BufReader::new(read_socket_half);
+            let mut line = String::new();
+
+            loop {
+                let bytes_read = reader
+                    .read_line(&mut line)
+                    .await
+                    .expect("Error reading data.");
+
+                if bytes_read == 0 {
+                    println!("Closed connection with {} on port {}.", addr.ip(), addr.port());
+                    break;
+                }
+
+                write_socket_half
+                    .write_all(line.as_bytes())
+                    .await
+                    .expect("Error echoing data.");
+
+                line.clear();
+            }
+        });
     }
 }
